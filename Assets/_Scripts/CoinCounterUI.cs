56487fcbc50540901fe487f25074641df7caf991
Alex Argatoff <alexargatoff@gmail.com>
2025-03-09 12:30:12 -0700

    [AU] Implemented animated coin counter

---
+++ Assets/_Scripts/CoinCounterUI.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+using System.Collections;
+
+public class CoinCounterUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI current;
+    [SerializeField] private TextMeshProUGUI toUpdate;
+    [SerializeField] private Transform coinTextContainer;
+    [SerializeField] private float duration;
+    [SerializeField] private Ease animationCurve;
+    private float containerInitPosition;
+    private float moveAmount;
+
+    private void Start()
+    {
+        Canvas.ForceUpdateCanvases();
+        current.SetText("0");
+        toUpdate.SetText("0");
+        containerInitPosition = coinTextContainer.localPosition.y;
+        moveAmount = current.rectTransform.rect.height;
+    }
+
+    public void UpdateScore(int score)
+    {
+        toUpdate.SetText($"{score}");
+        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration).SetEase(animationCurve);
+        StartCoroutine(ResetCoinContainer(score));
+    }
+
+    private IEnumerator ResetCoinContainer(int score)
+    {
+        yield return new WaitForSeconds(duration);
+        current.SetText($"{score}");
+        Vector3 localPosition = coinTextContainer.localPosition;
+        coinTextContainer.localPosition = new Vector3(localPosition.x, containerInitPosition, localPosition.z);
+    }
+}
